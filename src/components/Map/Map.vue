f6a79f4e94ef627d5f9ea638e8a8b8072051d451
Paul PERNALON <paul.pernalon@centrale-marseille.fr>
2021-10-17 20:10:59 +0200

    [Map] score computing

--- src/components/Map/Map.vue
+++ src/components/Map/Map.vue
@@ -8,82 +8,11 @@
 
 <script>
 import mapConstants from "/src/components/Map/MapConstants"
-// import MapHttpServices from "../../http/MapHttpServices"
+import {reactive} from "vue";
 
-const pickColorPool = (value) => {
-  let envColors
-  switch (value){
-    case mapConstants.PLAIN_ENV:
-      envColors = mapConstants.PLAIN_COLORS
-      break
-    case mapConstants.FOREST_ENV:
-      envColors = mapConstants.FOREST_COLORS
-      break
-    case mapConstants.MOUNTAIN_ENV:
-      envColors = mapConstants.MOUNTAIN_COLORS
-      break
-    case mapConstants.BEACH_ENV:
-      envColors = mapConstants.BEACH_COLORS
-      break
-    case mapConstants.LAKE_ENV:
-      envColors = mapConstants.LAKE_COLORS
-      break
-    default:
-      envColors = mapConstants.PLAIN_COLORS
-  }
-  return envColors
-}
-
-const drawRectOnCanvas = (col, row, color, context) => {
-  context.fillStyle = color
-  context.fillRect(
-      col * mapConstants.TILE_SIZE,
-      row * mapConstants.TILE_SIZE,
-      mapConstants.TILE_SIZE,
-      mapConstants.TILE_SIZE
-  )
-}
-
-const getSizeBuilding = (buildingType) => {
-  return mapConstants.SIZE_BUILDING[buildingType]
-}
-
-const getBuildingImageSource = (buildingType) => {
-  return mapConstants.SRC_BUILDINGS[buildingType]
-}
-const drawBuildingOnCanvas = (col, row, buildingType, context) => {
-  const imageSrc = getBuildingImageSource(buildingType)
-  const buildingSize = getSizeBuilding(buildingType)
-  const imageWidth = buildingSize[1] * mapConstants.TILE_SIZE
-  const imageHeight = buildingSize[0] * mapConstants.TILE_SIZE
-  const imageToDraw = new Image(imageWidth, imageHeight)
-  imageToDraw.src = imageSrc
-
-  context.drawImage(
-      imageToDraw,
-      col * mapConstants.TILE_SIZE,
-      row * mapConstants.TILE_SIZE,
-      imageWidth,
-      imageHeight
-  )
-}
-
-const computeInfluenceSphere = (col, row, buildingType) => {
-  const range = mapConstants.INFLUENCE_RANGES[buildingType]
-  const squaresToFill = []
-  for (let rowRange = 0; rowRange < range; rowRange++){
-    for (let colRange = 0; colRange < range; colRange++){
-      const dist = Math.sqrt(Math.pow(colRange, 2) + Math.pow(rowRange, 2))
-      if (dist < range){
-        squaresToFill.push([col + colRange, row + rowRange])
-        squaresToFill.push([col - colRange, row - rowRange])
-        squaresToFill.push([col + colRange, row - rowRange])
-        squaresToFill.push([col - colRange, row + rowRange])
-      }
-    }
-  }
-  return squaresToFill
-}
+const state = reactive({
+  buildings: []
+})
 
 export default {
   name: 'Map',
@@ -91,26 +20,35 @@ export default {
     map: Array,
     typeSelected: String,
   },
+
   data() {
     return {
-      buildings: [
-        {id: 1, type: "manoir"}
-      ],
       mapWidth: mapConstants.TILE_SIZE * mapConstants.NUMBER_TILE_COL,
       mapHeight: mapConstants.TILE_SIZE * mapConstants.NUMBER_TILE_ROW,
     }
   },
+
   mounted() {
     this.buildMap(this.map)
-    // const onResolve = (res) => {
-    //   this.buildMap(res.data)
-    // }
-    // const onError = (err) => {
-    //   console.log(err)
-    // }
-    // MapHttpServices.generateMap(onResolve, onError)
+    this.initBuildings()
   },
+
   methods: {
+    initBuildings(){
+      const startBuildings = []
+      for (let rowIndex = 0; rowIndex < mapConstants.NUMBER_TILE_ROW; rowIndex++ ){
+        const rowBuildings = []
+        for (let colIndex = 0; colIndex < mapConstants.NUMBER_TILE_COL; colIndex++ ){
+          rowBuildings.push({
+            id: 'none',
+            type: 'none',
+          })
+        }
+        startBuildings.push(rowBuildings)
+      }
+      state.buildings = startBuildings
+    },
+
     buildMap(generatedMap) {
       const canvas = this.$refs["backgroundMap"]
       const context2D = canvas.getContext("2d")
@@ -123,21 +61,21 @@ export default {
           const indexColor = Math.floor(Math.random() * (maxIndexColor - minIndexColor + 1)) + minIndexColor
 
           /* pick env colors */
-          const envColors = pickColorPool(col)
+          const envColors = this.pickColorPool(col)
 
-          drawRectOnCanvas(colIndex, rowIndex, envColors[indexColor], context2D)
+          this.drawRectOnCanvas(colIndex, rowIndex, envColors[indexColor], context2D)
         })
       })
     },
 
-    onMouseOut(){
+    onMouseOut() {
       /* reset hover canvas */
       const canvas = this.$refs["hoverCanvas"]
       const context2D = canvas.getContext("2d")
       context2D.clearRect(0, 0, this.mapWidth, this.mapHeight)
     },
 
-    onMouseMove(eventMouseMove){
+    onMouseMove(eventMouseMove) {
       /* compute row and col numbers */
       const rectCanvas = this.$refs["hoverCanvas"].getBoundingClientRect()
       let row = Math.floor((eventMouseMove.clientY - rectCanvas.top)/mapConstants.TILE_SIZE)
@@ -163,23 +101,214 @@ export default {
       hoverContext.clearRect(0, 0, this.mapWidth, this.mapHeight)
 
       /* draw influence */
-      const buildingType = "TownCenter" // TODO change with props after
-      const buildingSize = getSizeBuilding(buildingType)
+      const buildingType = "Manor" // TODO change with props after
 
-      let squaresToFill
-      if (buildingSize[0] > 1 && buildingSize[1] > 1){
-        const centerBuildingRow = row + (buildingSize[0]-1)/2
-        const centerBuildingCol = col + (buildingSize[1]-1)/2
-        squaresToFill = computeInfluenceSphere(centerBuildingCol, centerBuildingRow, buildingType)
-      } else {
-        squaresToFill = computeInfluenceSphere(col, row, buildingType)
+      const squaresToFill = this.computeInfluenceSphere(col, row, buildingType)
+
+      let influenceColor = mapConstants.INFLUENCE_COLOR
+      const currentEnv = this.getEnvBuilding(col, row, buildingType)
+      const envScore = this.getEnvScore(currentEnv, buildingType)
+      if (envScore === 'notAllowed'){
+        influenceColor = mapConstants.NOT_ALLOWED_COLOR
       }
+
       squaresToFill.forEach((square) => {
-        drawRectOnCanvas(square[0], square[1], mapConstants.INFLUENCE_COLOR, hoverContext)
+        this.drawRectOnCanvas(square[0], square[1], influenceColor, hoverContext)
       })
 
+      const score = this.computeScore(col, row, buildingType, state.buildings)
+
+      this.drawScoreOnCanvas(col, row, score, hoverContext)
+
       /* draw building */
-      drawBuildingOnCanvas(col, row, buildingType, hoverContext)
+      this.drawBuildingOnCanvas(col, row, buildingType, hoverContext)
+    },
+
+    pickColorPool(value) {
+      let envColors
+      switch (value){
+        case mapConstants.PLAIN_ENV:
+          envColors = mapConstants.PLAIN_COLORS
+          break
+        case mapConstants.FOREST_ENV:
+          envColors = mapConstants.FOREST_COLORS
+          break
+        case mapConstants.MOUNTAIN_ENV:
+          envColors = mapConstants.MOUNTAIN_COLORS
+          break
+        case mapConstants.BEACH_ENV:
+          envColors = mapConstants.BEACH_COLORS
+          break
+        case mapConstants.LAKE_ENV:
+          envColors = mapConstants.LAKE_COLORS
+          break
+        default:
+          envColors = mapConstants.PLAIN_COLORS
+      }
+      return envColors
+    },
+
+    drawBuildingOnCanvas(col, row, buildingType, context) {
+      const imageSrc = this.getBuildingImageSource(buildingType)
+      const buildingSize = this.getSizeBuilding(buildingType)
+      const imageWidth = buildingSize[1] * mapConstants.TILE_SIZE
+      const imageHeight = buildingSize[0] * mapConstants.TILE_SIZE
+      const imageToDraw = new Image(imageWidth, imageHeight)
+      imageToDraw.src = imageSrc
+
+      context.drawImage(
+          imageToDraw,
+          col * mapConstants.TILE_SIZE,
+          row * mapConstants.TILE_SIZE,
+          imageWidth,
+          imageHeight
+      )
+    },
+
+    drawRectOnCanvas(col, row, color, context) {
+      context.fillStyle = color
+      context.fillRect(
+          col * mapConstants.TILE_SIZE,
+          row * mapConstants.TILE_SIZE,
+          mapConstants.TILE_SIZE,
+          mapConstants.TILE_SIZE
+      )
+    },
+
+    drawScoreOnCanvas(col, row, score, context) {
+      context.font = "14px Slackey"
+      context.fillStyle = 'black'
+      context.fillText(
+          score.toString(),
+          (col + 1/2) * mapConstants.TILE_SIZE,
+          (row + 3) * mapConstants.TILE_SIZE
+      )
+    },
+
+    computeDist(dx, dy) {
+      return Math.sqrt(Math.pow(dy, 2) + Math.pow(dx, 2))
+    },
+
+    computeBuildingCenter(col, row, buildingWidth, buildingHeight){
+      const centerBuildingRow = row + (buildingHeight - 1) / 2
+      const centerBuildingCol = col + (buildingWidth - 1) / 2
+
+      return [centerBuildingCol, centerBuildingRow]
+    },
+
+    computeInfluenceSphere(col, row, buildingType) {
+
+      const buildingSize = this.getSizeBuilding(buildingType)
+
+      const [centerBuildingCol, centerBuildingRow] = this.computeBuildingCenter(col, row,  buildingSize[1], buildingSize[0])
+
+      const range = mapConstants.INFLUENCE_RANGES[buildingType]
+      const squaresToFill = []
+      for (let rowRange = 0; rowRange < range; rowRange++){
+        for (let colRange = 0; colRange < range; colRange++){
+          const dist = this.computeDist(rowRange, colRange);
+          if (dist < range){
+            squaresToFill.push([centerBuildingCol + colRange, centerBuildingRow + rowRange])
+            squaresToFill.push([centerBuildingCol - colRange, centerBuildingRow - rowRange])
+            squaresToFill.push([centerBuildingCol + colRange, centerBuildingRow - rowRange])
+            squaresToFill.push([centerBuildingCol - colRange, centerBuildingRow + rowRange])
+          }
+        }
+      }
+      return squaresToFill
+    },
+
+    getBuildingImageSource(buildingType) {
+      return mapConstants.SRC_BUILDINGS[buildingType]
+    },
+
+    getSizeBuilding(buildingType) {
+      return mapConstants.SIZE_BUILDING[buildingType]
+    },
+
+    getEnvScore(env, buildingType){
+      return mapConstants.SCORE_ENV[env][buildingType]
+    },
+
+    getEnv(col, row){
+      let env
+      switch (this.map[row][col]){
+        case mapConstants.PLAIN_ENV:
+          env = 'plain'
+          break
+        case mapConstants.FOREST_ENV:
+          env = 'forest'
+          break
+        case mapConstants.MOUNTAIN_ENV:
+          env = 'mountain'
+          break
+        case mapConstants.BEACH_ENV:
+          env = 'beach'
+          break
+        case mapConstants.LAKE_ENV:
+          env = 'water'
+          break
+        default:
+          env = 'plain'
+      }
+      return env
+    },
+
+    getEnvBuilding(col, row, buildingType){
+      let env = []
+      const buildingSize = this.getSizeBuilding(buildingType)
+      const buildingWidth = buildingSize[1]
+      const buildingHeight = buildingSize[0]
+
+      for (let colIndex = 0; colIndex < buildingHeight; colIndex++){
+        for (let rowIndex = 0; rowIndex < buildingWidth; rowIndex++){
+          env.push(this.getEnv(col + colIndex, row + rowIndex))
+        }
+      }
+
+      let finalEnv;
+      if (env.includes('plain')){
+        finalEnv = 'plain'
+      }
+      if (env.includes('forest')){
+        finalEnv = 'forest'
+      }
+      if (env.includes('mountain')){
+        finalEnv = 'mountain'
+      }
+      if (env.includes('beach')){
+        finalEnv = 'beach'
+      }
+      if (env.includes('water')){
+        finalEnv = 'water'
+      }
+
+      return finalEnv
+    },
+
+    computeScore(col, row, buildingType, buildingsArray)  {
+      const envBuiltOn = this.getEnvBuilding(col, row, buildingType)
+      const range = mapConstants.INFLUENCE_RANGES[buildingType]
+      const buildingsId = []
+
+      let score = this.getEnvScore(envBuiltOn, buildingType)
+      if (score === 'notAllowed'){
+        score = ''
+      } else {
+        buildingsArray.forEach((buildingsRow, buildingsRowIndex) => {
+          buildingsRow.forEach((building, buildingColIndex) => {
+            const dx = Math.abs(row - buildingsRowIndex)
+            const dy = Math.abs(col - buildingColIndex)
+            const dist = this.computeDist(dx, dy)
+            if (dist < range && !buildingsId.includes(building.id) && building.id !== 'none'){
+              buildingsId.push(building.id)
+              const toAdd = mapConstants.SCORE_BUILDINGS[buildingType][building.type]
+              score += toAdd
+            }
+          })
+        })
+      }
+      return score
     }
   }
 }
