6a2b003a0cd98a6eb4575e26cf8f6c29f975f7c3
Romain Camisard <romain.camisard@centrale-marseille.fr>
2021-10-21 19:28:47 +0200

    Romain - Update popup fin partie

--- src/components/Modal/Modal.vue
+++ src/components/Modal/Modal.vue
@@ -2,7 +2,6 @@
   <div class="modal-backdrop">
     <div class="modal">
       <header class="modal-header">
-        <p> Terminer la partie </p>
         <button type="button" class="btn-close" @click="close"> x </button>
       </header>
 
@@ -13,13 +12,12 @@
       </section>
       <section class="modal-body">
         <slot name="body">
-          <label for="inputUsername">Username </label>
-          <input type="text" id="inputUsername" name="Username" required>
+          <div id="username"> Username </div>
+          <input type="text" id="inputUsername" name="Username" @change="updateUsername" v-model="userName">
         </slot>
        </section>
-      <footer class="modal-footer">
-        <button type="button" class="btn" @click="close"> Retour </button>
-        <button type="button" class="btn" @click="postRankings"> Terminer la partie</button>
+      <footer class="modal-body">
+        <button type="button" id="btn-valider" @click="postRankings"> Valider </button>
       </footer>
     </div>
   </div>
@@ -36,6 +34,9 @@ import RankingHttpServices from '../../http/RankingHttpServices';
         components:{
             'ScoreBar' : ScoreBar,
         },
+        mounted(){
+          document.getElementById("btn-valider").disabled=true
+        },
         methods: {
         close() {
             this.$emit('close');
@@ -51,6 +52,17 @@ import RankingHttpServices from '../../http/RankingHttpServices';
           let username = document.getElementById("inputUsername").value
           RankingHttpServices.postRankingData(username, this.score, onResolve, onError) 
         },
+
+        updateUsername() {
+          let userName = document.getElementById("inputUsername").value
+          let btnValider = document.getElementById("btn-valider")
+          console
+          if (userName === "") {
+            btnValider.disabled=true;
+          } else {
+            btnValider.disabled=false
+          }
+        }
         }, 
     };
 </script>
@@ -63,40 +75,34 @@ import RankingHttpServices from '../../http/RankingHttpServices';
     left: 0;
     right: 0;
     z-index: 3;
-    background-color: rgba(0, 0, 0, 0.3);
     display: flex;
     justify-content: center;
     align-items: center;
+    backdrop-filter: blur(5px);
   }
 
   .modal {
+    height: 40vh;
+    width: 25vw;
     background: #FFFFFF;
-    box-shadow: 2px 2px 20px 1px;
     overflow-x: auto;
     display: flex;
     flex-direction: column;
+    box-shadow: 0 0 15px rgb(0 0 0 / 50%)
   }
 
-  .modal-header,
-  .modal-footer {
-    padding: 15px;
-  }
 
   .modal-header {
     position: relative;
-    border-bottom: 1px solid #eeeeee;
-    text-align: center;
-  }
+    padding: 5% 0;
+      }
 
-  .modal-footer {
-    border-top: 1px solid #eeeeee;
-    flex-direction: column;
-    justify-content: flex-end;
-  }
 
   .modal-body {
     position: relative;
-    padding: 20px 10px;
+    padding: 20px 15px;
+    margin-left: auto;
+    margin-right: auto;
   }
 
   .btn-close {
@@ -104,16 +110,50 @@ import RankingHttpServices from '../../http/RankingHttpServices';
     top: 0;
     right: 0;
     border: none;
+    color: #008CFF;
     font-size: 20px;
-    padding: 10px;
+    padding: 0px 10px;
     cursor: pointer;
     font-weight: bold;
     background: transparent;
   }
-
-  .btn {
+ #btn-valider:disabled{
+   background: grey;
+}
+  #btn-valider {
+    width: 30vh;
+    height: 5vh;
     color: white;
-    background: #008CFF;
-    font-family: inherit;
+    background: #008CCF;
+    font-family: 'Slackey';
+    font-size: 2em;
+    letter-spacing: 0.15em;
+    border: none;
+    cursor: pointer;
   }
+
+ 
+
+  input {
+  width: 20vw;
+  height: 5vh;
+  letter-spacing: 0.1em;
+  font-size: 1.5em;
+  color: #008CFF;
+  text-decoration: none;
+  border : solid #008CFF;
+  text-align: left;
+  padding: 1%;
+  float: right;
+}
+
+#username {
+  font-size: 1em;
+  position: absolute;
+  background: white;
+  top: 10px;
+  left: 20px;
+  padding: 0px 10px;
+}
+
 </style>
